4ef27a99eda0d9f8e6137b4080337a7c965f32a1
bentonwong <bentonwong@gmail.com>
2017-12-04 16:01:34 -0800

    fix auth bugs

--- services/passport.js
+++ services/passport.js
@@ -13,7 +13,17 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User ({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            //we already have a record with a given profile Id
+            done(null, existingUser);
+          } else {
+            new User ({ googleId: profile.id })
+              .save()
+              .then(user => done(null, user));
+          }
+        });
     }
   )
 );
