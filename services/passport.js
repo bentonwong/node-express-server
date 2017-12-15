e7c4885fce40be1b1c3db369223b86f7a3b2ee9f
bentonwong <bentonwong@gmail.com>
2017-12-14 23:30:02 -0800

    add login/logout button to header

--- services/passport.js
+++ services/passport.js
@@ -10,11 +10,10 @@ passport.serializeUser((user, done) => {
 })
 
 passport.deserializeUser((id, done) => {
-  User.findById(id)
-    .then(user => {
+  User.findById(id).then(user => {
       done(null, user);
-    })
-})
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -25,13 +24,14 @@ passport.use(
       proxy: true
     },
     async (accessToken, refreshToken, profile, done) => {
-      const existingUser = await User.findOne({ googleId: profile.id })
+      const existingUser = await User.findOne({ googleId: profile.id });
+
       if (existingUser) {
-        done(null, existingUser);
-      } else {
-        const user = await new User ({ googleId: profile.id }).save();
-        done(null, user);
+        return done(null, existingUser);
       }
+
+      const user = await new User ({ googleId: profile.id }).save();
+      done(null, user);
     }
   )
 );
