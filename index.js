aeb7c54484063833b98c7a9163045af45a964b67
bentonwong <bentonwong@gmail.com>
2017-12-23 10:57:41 -0800

    add Survey model

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const passport = require('passport');
 const bodyParser = require('body-parser');
 const keys = require('./config/keys');
 require('./models/User');
+require('./models/Survey');
 require('./services/passport');
 
 mongoose.Promise = global.Promise;
@@ -37,5 +38,3 @@ if (process.env.NODE_ENV === 'production') {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
-//update index.js file
