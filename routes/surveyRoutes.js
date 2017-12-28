030056f52b0836214bb9d5da819eef50c3e1f47d
bentonwong <bentonwong@gmail.com>
2017-12-27 20:57:49 -0800

    add Mailer

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const requireLogin = require ('../middlewares/requireLogin');
 const requireCredits = require ('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 
 const Survey = mongoose.model('surveys');
 
@@ -12,27 +14,11 @@ module.exports = app => {
       title,
       subject,
       body,
-      recipients: recipients.split(',').map(email => ({ email })),
+      recipients: recipients.split(',').map(email => ({ email: email.trim() })),
       _user: req.user.id,
       dateSent: Date.now()
     });
+
+    const mailer = new Mailer(survey, surveyTemplate(survey));
   });
 };
-
-/*
-const charge = await stripe.charges.create({
-  amount: 500,
-  currency: 'usd',
-  description: '$5 for 5 credits',
-  source: req.body.id
-});
-title: String,
-body: String,
-subject: String,
-recipients: [RecipientSchema],
-yes: { type: Number, default: 0 },
-no: { type: Number, default: 0 },
-_user: { type: Schema.Types.ObjectId, ref: 'User' },
-dateSent: Date,
-lastResponded: Date
-*/
