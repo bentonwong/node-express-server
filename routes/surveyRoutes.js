4cf1e65ce9bb1a8e424c5cfce4c18694b5f4518e
bentonwong <bentonwong@gmail.com>
2018-01-10 12:22:00 -0800

    add mongodb update query in survey routes

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,7 +1,6 @@
 const _ = require('lodash');
 const Path = require('path-parser');
 const { URL } = require('url');
-
 const mongoose = require('mongoose');
 const requireLogin = require ('../middlewares/requireLogin');
 const requireCredits = require ('../middlewares/requireCredits');
@@ -11,13 +10,13 @@ const surveyTemplate = require('../services/emailTemplates/surveyTemplate');
 const Survey = mongoose.model('surveys');
 
 module.exports = app => {
-  app.get('/api/surveys/thanks', (req, res) => {
+  app.get('/api/surveys/:surveyId/:choice', (req, res) => {
     res.send('Thanks!');
   })
 
   app.post('/api/surveys/webhooks', (req, res) => {
     const p = new Path('/api/surveys/:surveyId/:choice');
-    const events = _.chain(req.body)
+    _.chain(req.body)
       .map(({ email, url }) => {
         const match = p.test(new URL(url).pathname);
         if (match) {
@@ -26,9 +25,23 @@ module.exports = app => {
       })
       .compact()
       .uniqBy('email', 'surveyId')
+      .each(({ surveyId, email, choice }) => {
+        Survey.updateOne(
+          {
+            _id: surveyId,
+            recipients: {
+              $elemMatch: { email: email, responded: false }
+            }
+          },
+          {
+            $inc: { [choice]: 1 },
+            $set: { 'recipients.$.responded': true },
+            lastResponded: new Date()
+          }
+        ).exec();
+      })
       .value();
 
-    console.log(events);
     res.send({});
   });
 
